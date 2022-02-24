83ff05cefbd4acfed43c432c2dc4b981d82ff9f7
yhsol <loshY244110@gmail.com>
2022-02-25 07:49:27 +0900

    init

---
+++ src/_data/sampleJson.ts
@@ -0,0 +1,11 @@
+export const sampleJSON = {
+  name: "Leanne Graham",
+  username: "Bret",
+  email: "Sincere@april.biz",
+  "address.street": "Kulas Light",
+  "address.suite": "Apt. 556",
+  "address.city": "Gwenborough",
+  "address.zipcode": "92998-3874",
+  "address.geo.lat": "-37.3159",
+  "address.geo.lng": "81.1496",
+};
